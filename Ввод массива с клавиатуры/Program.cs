619d398d5627f12b70c5736a54906338d0d25bea
ayazk <ayazkinmail@gmail.com>
2022-12-06 17:58:24 +0500

    Добавьте файлы проекта.

---
+++ Ввод массива с клавиатуры/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ввод_массива_с_клавиатуры
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов массива: ");
+            int elementsCount = int.Parse(Console.ReadLine());
+
+            int[] AyazkinArray = new int[elementsCount];
+
+            for (int i = 0; i < elementsCount; i++)
+            {
+                Console.Write($"Введите {i + 1} элемент массива: ");
+                AyazkinArray[i] = int.Parse(Console.ReadLine()); 
+            }
+
+
+            Console.Write("Ваш массив: ");
+            for(int i = 0; i < AyazkinArray.Length; i++)
+            {       
+            Console.Write(AyazkinArray[i] + " ");
+            }
+        }
+    }
+}
